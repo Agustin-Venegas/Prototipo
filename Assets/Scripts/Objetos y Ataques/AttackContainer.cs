0f40604f09a5e77d22f64dd4b97cbbc421408a9b
Agustin-Venegas <avenegas19@alumnos.utalca.cl>
2020-10-05 00:23:03 -0300

    ProbandoLinux Se pusieron scripts, imagenes y prefabs

---
+++ Assets/Scripts/Objetos y Ataques/AttackContainer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//muy parecido al PlayerAttack, deberia hacer una clase/struct ataque
+//esta se diferencia en que los enemigos la utilizan tambien
+
+public class AttackContainer : MonoBehaviour
+{
+    [Header("Objeto a Spawnear")]
+    public GameObject bulletPrefab;
+    public bool IsMelee; //si es melee, va a querer acercarse al jugador antes de atacar
+
+    [Header("Variables del ataque")]
+    public float Cooldown = 0.5f;
+    public bool UsesAmmo = false; //si el ataque usa municion
+    public int Max_Ammo; //solo se usa si el bool anterior es true
+
+    int ammo;
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (UsesAmmo) ammo = Max_Ammo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0) timer -= Time.deltaTime;
+    }
+
+    public int GetAmmo() { return ammo; }
+}
