3ac445a5bbe0b2a1cc59711e11d605915b62185c
Agustin-Venegas <68612141+Agustin-Venegas@users.noreply.github.com>
2020-12-01 16:23:07 -0300

    Mejoras varias
    Sistema de dialogos q puedes saltar
    unos tiles bien chingones
    unos scripts pa cambios de escena
    mas o menos eso

---
+++ Assets/Scripts/Logic/DialogSystem/ExecuteOnStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExecuteOnStart : MonoBehaviour
+{
+    public UnityEvent toDo;
+	
+    void Start()
+    {
+        toDo.Invoke();
+		enabled = false;	
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Logic/DialogSystem/FadeScene.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class FadeScene : MonoBehaviour
+{
+	
+	public GameObject cuadrao;
+	public UnityEvent OnFinish;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void StartFade(bool pa_negro) 
+	{
+		Task t = new Task(Cambiar(pa_negro,1));
+		t.Finished += delegate(bool man) 
+		{
+			OnFinish.Invoke();
+		};
+	}
+	
+	public IEnumerator Cambiar(bool pa_negro = true, int speed = 5) 
+	{
+		Color c = cuadrao.GetComponent<Image>().color;
+		float amount;
+		
+		if (pa_negro) 
+		{
+			while (cuadrao.GetComponent<Image>().color.a < 1) 
+			{
+				amount = c.a + (speed * Time.deltaTime);
+				
+				c = new Color(c.r, c.g, c.b, amount);
+				cuadrao.GetComponent<Image>().color = c;
+				yield return null;
+			}
+			
+			OnFinish.Invoke();
+		} else 
+		{
+			while (cuadrao.GetComponent<Image>().color.a > 0) 
+			{
+				amount = c.a - (speed * Time.deltaTime);
+				
+				c = new Color(c.r, c.g, c.b, amount);
+				cuadrao.GetComponent<Image>().color = c;
+				yield return null;
+			}
+			
+			OnFinish.Invoke();
+		}
+	}
+}
