1bcdd7f5bf94449d37dcee9b4cd1710726e8a9f5
Agustin-Venegas <68612141+Agustin-Venegas@users.noreply.github.com>
2020-12-15 15:24:39 -0300

    Cambios a los sprites
    ahora las animaciones rotan en su centro

--- Assets/Scripts/Logic/CamControl.cs
+++ Assets/Scripts/Logic/CamControl.cs
@@ -5,31 +5,31 @@ using UnityEngine;
 public class CamControl : MonoBehaviour
 {
     public Vector3 offset;
-    public Transform follow;
+    public Transform follow = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (follow == null) 
-		{
-			
-		}
+		
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        transform.position = follow.position + offset;
+        if (follow != null) 
+		{
+			transform.position = follow.position + offset;
 
-        if (Input.GetKey("left shift"))
-        {
+			if (Input.GetKey("left shift"))
+			{
 
-            Vector3 screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
-            screenPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+				Vector3 screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+				screenPosition = Camera.main.ScreenToWorldPoint(screenPosition);
 
 
-            transform.position = follow.position + offset + (screenPosition-transform.position);
-        }
+				transform.position = follow.position + offset + (screenPosition-transform.position);
+			}
+		}
     }
 }
